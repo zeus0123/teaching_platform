191e96a3669b1f057e3ecee406cb69e69b0fed37
zeus0123 <37953384+zeus0123@users.noreply.github.com>
2021-07-18 16:50:57 +0530

    setup fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '../js/bootstrap_js_files.js'  
+import '../js/bootstrap_js_files.js'
+import "@fortawesome/fontawesome-free/js/all";  
 
 Rails.start()
 Turbolinks.start()
