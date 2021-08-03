c65c2690af7d17d4c7681bedb9cb29e02d63e271
Trishan Ghosh <ghoshtrishan73@gmail.com>
2021-08-03 15:58:22 +0530

    setup friendly id

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "haml-rails", "~> 2.0"
 gem 'simple_form'
 gem 'faker'
 gem 'devise'
+gem 'friendly_id', '~> 5.4.0'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/models/course.rb
+++ app/models/course.rb
@@ -8,5 +8,13 @@ class Course < ApplicationRecord
   end
 
   has_rich_text :description
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  # friendly_id :generated_slug, use: :slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? friendly_id : SecureRandom.hex(4)
+  # end
 
 end
